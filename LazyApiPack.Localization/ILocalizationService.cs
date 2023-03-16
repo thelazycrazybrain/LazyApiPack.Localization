5c4323ebcaecc7648ff1b20c984d7eeca4df0f4f
Jodie Watson <jodie@jodiewatson.net>
2023-03-16 18:47:31 +0100

    Added support to load translations from an embedded resource.

--- LazyApiPack.Localization/ILocalizationService.cs
+++ LazyApiPack.Localization/ILocalizationService.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.ObjectModel;
 using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
 
 namespace LazyApiPack.Localization
 {
@@ -20,6 +21,14 @@ namespace LazyApiPack.Localization
         /// <param name="searchPattern">If you work with directories, this filter can be used to select specific localization files (e.g. *.json)</param>
         /// <param name="options">If you work with directories, you can specify the search options (e.g. include subdirectories etc.)</param>
         void AddLocalizations([NotNull] string[] localizationDirectories, string? searchPattern = null, EnumerationOptions? options = null);
+
+        /// <summary>
+        /// Adds localization files from an assembly.
+        /// </summary>
+        /// <param name="assembly">Assembly that contains the localization resources.</param>
+        /// <param name="localizationNamespaces">Namespaces that include localization resources.</param>
+        /// <param name="searchPattern">Search pattern that finds resources in the assembly.</param>
+        void AddLocalizations([NotNull] Assembly assembly, [NotNull] string[] localizationNamespaces, string? searchPattern = null);
         /// <summary>
         /// Gets or sets the current language.
         /// </summary>
